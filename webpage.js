7675f94c13be3ad6cbc33c3c870f95b91be67288
rsk9607 <108059528+rsk9607@users.noreply.github.com>
2022-08-20 15:49:11 +0530

    last

--- webpage.js
+++ webpage.js
@@ -1,8 +1,10 @@
 function MyImg(){
     var x = document.getElementById("name").value;
     var y = document.getElementById("phonenumber").value;
-    var z =document.getElementById("Amount").value;
-    var a =document.
-    getElementById("date").value;
+    var z = document.getElementById("Amount").value;
+    var a = document.getElementById("date").value;
     console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log(a);
 }
